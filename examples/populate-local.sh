b6727b61cef9775b3dbe3085882298a25c7a29e5
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2021-04-14 16:19:31 +0200

    copy examples from elucidate-server

--- examples/populate-local.sh
+++ examples/populate-local.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 EL_BASE=http://localhost:8080/annotation
-ACCEPT_HEADER='Accept: application/ld+json; profile="http://www.w3.org/ns/anno.jsonld"'
-CONTENT_TYPE_HEADER='Content-Type: application/ld+json; profile="http://www.w3.org/ns/anno.jsonld"'
+ACCEPT_HEADER=
+CONTENT_TYPE_HEADER=
 CONTAINER_URI=$EL_BASE/w3c/examples/
 
 # create container
 echo ">> create examples container"
 curl -X POST \
-     --data @create-container.json \
-     -H $ACCEPT_HEADER \
-     -H $CONTENT_TYPE_HEADER \
+     --data @container.json \
+     -H 'Accept: application/ld+json; profile="http://www.w3.org/ns/anno.jsonld"' \
+     -H 'Content-Type: application/ld+json; profile="http://www.w3.org/ns/anno.jsonld"' \
      -H 'Slug: examples' \
      $EL_BASE/w3c/
 echo
 
 # add annotations
 echo ">> add annotations"
-for j in create-annotation*.json; do
+for j in annotation*.json; do
   curl --data @$j \
-     -H $ACCEPT_HEADER \
-     -H $CONTENT_TYPE_HEADER \
+     -H 'Accept: application/ld+json; profile="http://www.w3.org/ns/anno.jsonld"' \
+     -H 'Content-Type: application/ld+json; profile="http://www.w3.org/ns/anno.jsonld"' \
      $CONTAINER_URI
 done
 echo
